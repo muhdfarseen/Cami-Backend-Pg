ddfe4be9f5038d73ae0f6d94f76e22a9229e08d0
Farseen <muhdfarseen@gmail.com>
2024-02-10 22:34:26 +0530

    feat: delete student api

--- app.js
+++ app.js
@@ -19,6 +19,9 @@ const db = mysql.createConnection({
     database: 'cami'
 });
 
+
+//Driver APP
+
 // Route to handle updating location
 app.post('/update-location', (req, res) => {
     const { bus_number, latitude, longitude } = req.body;
@@ -36,6 +39,48 @@ app.post('/update-location', (req, res) => {
         }
     );
 });
+
+
+
+
+
+
+
+//Admin APP
+
+// Route to fetch student details
+app.get('/students', (req, res) => {
+  db.query(
+      'SELECT * FROM student',
+      (error, results) => {
+          if (error) {
+              console.error('Error fetching student details:', error);
+              res.status(500).send('Error fetching student details');
+          } else {
+              res.status(200).json(results);
+          }
+      }
+  );
+});
+
+app.delete('/student/:register_number', (req, res) => {
+  const register_number = req.params.register_number;
+
+  db.query(
+      'DELETE FROM student WHERE register_number = ?',
+      [register_number],
+      (error, results) => {
+          if (error) {
+              console.error('Error deleting student:', error);
+              res.status(500).send('Error deleting student');
+          } else {
+              res.status(200).send('Student deleted successfully');
+          }
+      }
+  );
+});
+
+
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
