de019ad7b1ac6704517b3b1a06ff6000a5a83636
Farseen <muhdfarseen@gmail.com>
2024-02-11 23:55:52 +0530

    refactor: acc center

--- app.js
+++ app.js
@@ -12,54 +12,48 @@ app.use(express.json());
 const server = http.createServer(app);
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '',
-    database: 'cami'
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: '',
+  database: 'cami'
 });
 
 
-//Driver APP
+//----------------------------------------------------------Driver APP
 
 // Route to handle updating location
 app.post('/update-location', (req, res) => {
-    const { bus_number, latitude, longitude } = req.body;
+  const { bus_number, latitude, longitude } = req.body;
 
-    db.query(
-        'UPDATE location SET latitude = ?, longitude = ? WHERE bus_number = ?',
-        [latitude, longitude, bus_number],
-        (error, results) => {
-            if (error) {
-                console.error('Error updating location:', error);
-                res.status(500).send('Error updating location');
-            } else {
-                res.status(200).send('Location updated successfully');
-            }
-        }
-    );
+  db.query(
+    'UPDATE location SET latitude = ?, longitude = ? WHERE bus_number = ?',
+    [latitude, longitude, bus_number],
+    (error, results) => {
+      if (error) {
+        console.error('Error updating location:', error);
+        res.status(500).send('Error updating location');
+      } else {
+        res.status(200).send('Location updated successfully');
+      }
+    }
+  );
 });
 
-
-
-
-
-
-
-//Admin APP
+//----------------------------------------------------------Admin APP
 
 // Route to fetch student details
 app.get('/students', (req, res) => {
   db.query(
-      'SELECT * FROM student',
-      (error, results) => {
-          if (error) {
-              console.error('Error fetching student details:', error);
-              res.status(500).send('Error fetching student details');
-          } else {
-              res.status(200).json(results);
-          }
+    'SELECT * FROM student',
+    (error, results) => {
+      if (error) {
+        console.error('Error fetching student details:', error);
+        res.status(500).send('Error fetching student details');
+      } else {
+        res.status(200).json(results);
       }
+    }
   );
 });
 
@@ -67,38 +61,38 @@ app.delete('/student/:register_number', (req, res) => {
   const register_number = req.params.register_number;
 
   db.query(
-      'DELETE FROM student WHERE register_number = ?',
-      [register_number],
-      (error, results) => {
-          if (error) {
-              console.error('Error deleting student:', error);
-              res.status(500).send('Error deleting student');
-          } else {
-              res.status(200).send('Student deleted successfully');
-          }
+    'DELETE FROM student WHERE register_number = ?',
+    [register_number],
+    (error, results) => {
+      if (error) {
+        console.error('Error deleting student:', error);
+        res.status(500).send('Error deleting student');
+      } else {
+        res.status(200).send('Student deleted successfully');
       }
+    }
   );
 });
 
 // Route to fetch bus locations
 app.get('/bus-locations', (req, res) => {
   db.query(
-      'SELECT * FROM location',
-      (error, results) => {
-          if (error) {
-              console.error('Error fetching bus locations:', error);
-              res.status(500).send('Error fetching bus locations');
-          } else {
-              const busLocations = {};
-              results.forEach((row) => {
-                  busLocations[row.bus_number] = {
-                      latitude: row.latitude,
-                      longitude: row.longitude
-                  };
-              });
-              res.status(200).json(busLocations);
-          }
+    'SELECT * FROM location',
+    (error, results) => {
+      if (error) {
+        console.error('Error fetching bus locations:', error);
+        res.status(500).send('Error fetching bus locations');
+      } else {
+        const busLocations = {};
+        results.forEach((row) => {
+          busLocations[row.bus_number] = {
+            latitude: row.latitude,
+            longitude: row.longitude
+          };
+        });
+        res.status(200).json(busLocations);
       }
+    }
   );
 });
 
@@ -107,67 +101,64 @@ app.delete('/delete-student/:admission_year', (req, res) => {
   const admission_year = req.params.admission_year;
 
   db.query(
-      'DELETE FROM student WHERE admission_year = ?',
-      [admission_year],
-      (error, results) => {
-          if (error) {
-              console.error('Error deleting students of admission year:', error);
-              res.status(500).send('Error deleting students of admission year');
-          } else {
-              res.status(200).send('Students of admission year deleted successfully');
-          }
+    'DELETE FROM student WHERE admission_year = ?',
+    [admission_year],
+    (error, results) => {
+      if (error) {
+        console.error('Error deleting students of admission year:', error);
+        res.status(500).send('Error deleting students of admission year');
+      } else {
+        res.status(200).send('Students of admission year deleted successfully');
       }
+    }
   );
 });
-
 
 // Route to fetch delivered messages
 app.get('/delivered-messages', (req, res) => {
   db.query(
-      'SELECT * FROM message',
-      (error, results) => {
-          if (error) {
-              console.error('Error fetching student details:', error);
-              res.status(500).send('Error fetching student details');
-          } else {
-              res.status(200).json(results);
-          }
+    'SELECT * FROM message',
+    (error, results) => {
+      if (error) {
+        console.error('Error fetching student details:', error);
+        res.status(500).send('Error fetching student details');
+      } else {
+        res.status(200).json(results);
       }
+    }
   );
 });
-
-//Admin APP
 
 // Route to delete a specific message
 app.delete('/message/:msg_id', (req, res) => {
   const msg_id = req.params.msg_id;
 
   db.query(
-      'DELETE FROM message WHERE msg_id = ?',
-      [msg_id],
-      (error, results) => {
-          if (error) {
-              console.error('Error deleting message:', error);
-              res.status(500).send('Error deleting message');
-          } else {
-              res.status(200).send('Message deleted successfully');
-          }
+    'DELETE FROM message WHERE msg_id = ?',
+    [msg_id],
+    (error, results) => {
+      if (error) {
+        console.error('Error deleting message:', error);
+        res.status(500).send('Error deleting message');
+      } else {
+        res.status(200).send('Message deleted successfully');
       }
+    }
   );
 });
 
 // Route to delete all messages
 app.delete('/delete-all-messages', (req, res) => {
   db.query(
-      'DELETE FROM message',
-      (error, results) => {
-          if (error) {
-              console.error('Error deleting all messages:', error);
-              res.status(500).send('Error deleting all messages');
-          } else {
-              res.status(200).send('All messages deleted successfully');
-          }
+    'DELETE FROM message',
+    (error, results) => {
+      if (error) {
+        console.error('Error deleting all messages:', error);
+        res.status(500).send('Error deleting all messages');
+      } else {
+        res.status(200).send('All messages deleted successfully');
       }
+    }
   );
 });
 
@@ -176,36 +167,101 @@ app.post('/compose-message', (req, res) => {
   const { subject, description, 'rec-dept': recDept, 'rec-year': recYear } = req.body;
 
   db.query(
-      'INSERT INTO message (subject, description, `rec-dept`, `rec-year`) VALUES (?, ?, ?, ?)',
-      [subject, description, JSON.stringify(recDept), JSON.stringify(recYear)],
-      (error, results) => {
-          if (error) {
-              console.error('Error composing message:', error);
-              res.status(500).send('Error composing message');
-          } else {
-              res.status(200).send('Message composed and sent successfully');
-          }
+    'INSERT INTO message (subject, description, `rec-dept`, `rec-year`) VALUES (?, ?, ?, ?)',
+    [subject, description, JSON.stringify(recDept), JSON.stringify(recYear)],
+    (error, results) => {
+      if (error) {
+        console.error('Error composing message:', error);
+        res.status(500).send('Error composing message');
+      } else {
+        res.status(200).send('Message composed and sent successfully');
       }
+    }
   );
 });
 
-
 // Route to update student information
 app.put('/student/:register_number', (req, res) => {
-    console.log('Request Body:', req.body); // Log the request body
-    const register_number = req.params.register_number;
-    const { email_id, full_name, department, admission_year, bus_from, bus_number, pass_status, amount_paid, paid_on, pass_expires_on, dob } = req.body;
+  console.log('Request Body:', req.body); // Log the request body
+  const register_number = req.params.register_number;
+  const { email_id, full_name, department, admission_year, bus_from, bus_number, pass_status, amount_paid, paid_on, pass_expires_on, dob } = req.body;
   db.query(
-      'UPDATE student SET email_id = ?, full_name = ?, department = ?, admission_year = ?, bus_from = ?, bus_number = ?, pass_status = ?, amount_paid = ?, paid_on = ?, pass_expires_on = ?, `dob` = ? WHERE register_number = ?',
-      [email_id, full_name, department, admission_year, bus_from, bus_number, pass_status, amount_paid, paid_on, pass_expires_on, dob, register_number],
-      (error, results) => {
-          if (error) {
-              console.error('Error updating student:', error);
-              res.status(500).send('Error updating student');
-          } else {
-              res.status(200).send('Student updated successfully');
-          }
+    'UPDATE student SET email_id = ?, full_name = ?, department = ?, admission_year = ?, bus_from = ?, bus_number = ?, pass_status = ?, amount_paid = ?, paid_on = ?, pass_expires_on = ?, `dob` = ? WHERE register_number = ?',
+    [email_id, full_name, department, admission_year, bus_from, bus_number, pass_status, amount_paid, paid_on, pass_expires_on, dob, register_number],
+    (error, results) => {
+      if (error) {
+        console.error('Error updating student:', error);
+        res.status(500).send('Error updating student');
+      } else {
+        res.status(200).send('Student updated successfully');
       }
+    }
+  );
+});
+
+//----------------------------------------------------------Student APP
+
+// Route to handle user registration
+app.post('/register', (req, res) => {
+  const { register_number, email_id, full_name, department, admission_year, dob } = req.body;
+
+  db.query(
+    'INSERT INTO student (register_number, email_id, full_name, department, admission_year, dob) VALUES (?, ?, ?, ?, ?, ?)',
+    [register_number, email_id, full_name, department, admission_year, dob],
+    (error, results) => {
+      if (error) {
+        console.error('Error registering user:', error);
+        res.status(500).send('Error registering user');
+      } else {
+        res.status(200).send('User registered successfully');
+      }
+    }
+  );
+});
+
+// Route to handle user login
+app.post('/login', (req, res) => {
+  const registerNumber = req.body.registerNumber;
+  const dob = req.body.dob;
+
+  db.execute(
+    "SELECT * FROM student WHERE register_number = ? AND dob = ?",
+    [registerNumber, dob],
+    (err, result) => {
+      if (err) {
+        console.error('Error executing SQL query:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+        return;
+      }
+
+      if (result.length > 0) {
+        // If login is successful, return all student data
+        res.status(200).json(result[0]);
+      } else {
+        // If login fails, return a generic message
+        res.status(401).json({ message: "Invalid credentials" });
+      }
+    }
+  );
+});
+
+// Route to get message for specific year and dept
+app.get('/delivered-messages/:admission_year/:department', (req, res) => {
+  const { admission_year, department } = req.params;
+
+  // Query the database to fetch messages that match the user's admission year and department,
+  // as well as messages intended for all users
+  db.query(
+    'SELECT * FROM message WHERE (JSON_CONTAINS(`rec-year`, ?, "$") OR JSON_CONTAINS(`rec-year`, \'["All"]\', "$")) AND (JSON_CONTAINS(`rec-dept`, ?, "$") OR JSON_CONTAINS(`rec-dept`, \'["All"]\', "$"))',
+    [JSON.stringify(admission_year), JSON.stringify(department)],
+    (error, results) => {
+      if (error) {
+        console.error('Error fetching messages:', error);
+        res.status(500).send('Error fetching messages');
+      } else {
+        res.status(200).json(results);
+      }
+    }
   );
 });
 
@@ -216,5 +272,5 @@ app.put('/student/:register_number', (req, res) => {
 
 
 server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
--- cami.sql
+++ cami.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 11, 2024 at 09:15 AM
+-- Generation Time: Feb 11, 2024 at 04:17 PM
 -- Server version: 11.2.2-MariaDB
 -- PHP Version: 8.0.30
 
@@ -57,8 +57,8 @@ CREATE TABLE `message` (
   `subject` text DEFAULT NULL,
   `description` text DEFAULT NULL,
   `sent_time` timestamp NULL DEFAULT current_timestamp(),
-  `rec-year` text DEFAULT NULL,
-  `rec-dept` text DEFAULT NULL
+  `rec-year` text DEFAULT '["All"]',
+  `rec-dept` text DEFAULT '["All"]'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -66,7 +66,8 @@ CREATE TABLE `message` (
 --
 
 INSERT INTO `message` (`msg_id`, `subject`, `description`, `sent_time`, `rec-year`, `rec-dept`) VALUES
-(1, 'New Message', 'No Bus on Friday Feb 14', '2024-02-11 08:12:59', '[\'2020\',\'2024\',\'2022\']', '[\'IT\',\'CS\']');
+(3, 'hello', 'asdasdasdas', '2024-02-11 09:34:21', '[\"All\"]', '[\"All\"]'),
+(4, 'Hello World', 'This is a sample message to IT 2024', '2024-02-11 09:35:06', '[\"2024\"]', '[\"IT\"]');
 
 -- --------------------------------------------------------
 
@@ -86,31 +87,21 @@ CREATE TABLE `student` (
   `amount_paid` varchar(255) DEFAULT NULL,
   `paid_on` date DEFAULT NULL,
   `pass_expires_on` date DEFAULT NULL,
-  `password-DOB` text NOT NULL
+  `dob` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `student`
 --
 
-INSERT INTO `student` (`register_number`, `email_id`, `full_name`, `department`, `admission_year`, `bus_from`, `bus_number`, `pass_status`, `amount_paid`, `paid_on`, `pass_expires_on`, `password-DOB`) VALUES
-('2022004', 'bob.johnson@example.com', 'Bob Johnson', 'Civil Engineering', '2022', 'Campus', 4, 0, '90', '2023-02-10', '2023-08-10', 'password4-20010404'),
-('2022005', 'emma.wilson@example.com', 'Emma Wilson', 'Chemical Engineering', '2022', 'Campus', 5, 1, '130', '2023-02-15', '2023-08-15', 'password5-20010505'),
-('2022006', 'william.miller@example.com', 'William Miller', 'Aerospace Engineering', '2022', 'Campus', 6, 1, '105', '2023-02-20', '2023-08-20', 'password6-20010606'),
-('2022007', 'olivia.brown@example.com', 'Olivia Brown', 'Biomedical Engineering', '2022', 'Campus', 1, 1, '95', '2023-03-05', '2023-09-05', 'password7-20010707'),
-('2022008', 'noah.moore@example.com', 'Noah Moore', 'Computer Science', '2022', 'Campus', 2, 1, '140', '2023-03-10', '2023-09-10', 'password8-20010808'),
-('2022009', 'ava.taylor@example.com', 'Ava Taylor', 'Electrical Engineering', '2022', 'Campus', 3, 1, '100', '2023-03-15', '2023-09-15', 'password9-20010909'),
-('2022010', 'liam.anderson@example.com', 'Liam Anderson', 'Mechanical Engineering', '2022', 'Campus', 4, 1, '115', '2023-03-20', '2023-09-20', 'password10-20011010'),
-('2022011', 'sophia.thomas@example.com', 'Sophia Thomas', 'Civil Engineering', '2022', 'Campus', 5, 1, '95', '2023-04-05', '2023-10-05', 'password11-20011111'),
-('2022012', 'jacob.hall@example.com', 'Jacob Hall', 'Chemical Engineering', '2022', 'Campus', 6, 1, '105', '2023-04-10', '2023-10-10', 'password12-20011212'),
-('2022013', 'olivia.lewis@example.com', 'Olivia Lewis', 'Aerospace Engineering', '2022', 'Campus', 1, 1, '120', '2023-04-15', '2023-10-15', 'password13-20011313'),
-('2022014', 'ethan.lee@example.com', 'Ethan Lee', 'Biomedical Engineering', '2022', 'Campus', 2, 1, '110', '2023-04-20', '2023-10-20', 'password14-20011414'),
-('2022015', 'ava.moore@example.com', 'Ava Moore', 'Computer Science', '2022', 'Campus', 3, 1, '100', '2023-05-05', '2023-11-05', 'password15-20011515'),
-('2022016', 'oliver.robinson@example.com', 'Oliver Robinson', 'Electrical Engineering', '2022', 'Campus', 4, 1, '90', '2023-05-10', '2023-11-10', 'password16-20011616'),
-('2022017', 'amelia.king@example.com', 'Amelia King', 'Mechanical Engineering', '2022', 'Campus', 5, 1, '130', '2023-05-15', '2023-11-15', 'password17-20011717'),
-('2022018', 'matthew.scott@example.com', 'Matthew Scott', 'Civil Engineering', '2022', 'Campus', 6, 1, '110', '2023-05-20', '2023-11-20', 'password18-20011818'),
-('2022019', 'charlotte.allen@example.com', 'Charlotte Allen', 'Chemical Engineering', '2022', 'Campus', 1, 1, '105', '2023-06-05', '2023-12-05', 'password19-20011919'),
-('2022020', 'mia.young@example.com', 'Mia Young', 'Aerospace Engineering', '2022', 'Campus', 2, 1, '115', '2023-06-10', '2023-12-10', 'password20-20012020');
+INSERT INTO `student` (`register_number`, `email_id`, `full_name`, `department`, `admission_year`, `bus_from`, `bus_number`, `pass_status`, `amount_paid`, `paid_on`, `pass_expires_on`, `dob`) VALUES
+('2022014', 'ethan.lee@example.com', 'Dora', 'IT', '2022', 'Campus', 2, 1, '110', '2023-04-13', '2023-10-13', '123'),
+('2022015', 'ava.moore@example.com', 'Ava Moore', 'Computer Science', '2022', 'Campus', 3, 1, '100', '2023-05-04', '2023-11-04', '123'),
+('2022016', 'oliver.robinson@example.com', 'Oliver Robinson', 'Electrical Engineering', '2022', 'Campus', 4, 1, '90', '2023-05-09', '2023-11-09', '123'),
+('2022017', 'amelia.king@example.com', 'Amelia King', 'Mechanical Engineering', '2022', 'Campus', 5, 1, '130', '2023-05-14', '2023-11-14', '123'),
+('2022018', 'matthew.scott@example.com', 'Matthew Scott', 'Civil Engineering', '2022', 'Campus', 6, 1, '110', '2023-05-19', '2023-11-19', '123'),
+('2022019', 'charlotte.allen@example.com', 'Charlotte Allen', 'Chemical Engineering', '2022', 'Campus', 1, 1, '105', '2023-06-04', '2023-12-04', '123'),
+('2022020', 'mia.young@example.com', 'Mia Young', 'Aerospace Engineering', '2022', 'Campus', 2, 1, '115', '2023-06-09', '2023-12-09', '123');
 
 --
 -- Indexes for dumped tables
@@ -143,7 +134,7 @@ ALTER TABLE `student`
 -- AUTO_INCREMENT for table `message`
 --
 ALTER TABLE `message`
-  MODIFY `msg_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `msg_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
